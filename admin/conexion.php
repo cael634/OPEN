e32cae409882d9274dfad922fa7c160ee3b8aefb
cael634 <49290751+cael634@users.noreply.github.com>
2025-07-31 23:43:16 -0500

    subir todo

--- admin/conexion.php
+++
@@ -1,2 +0,0 @@
-<?php$conexion =mysqli_connect($host_mysql,$user_mysql,$pass_mysql,$db_mysql);
-mysqli_select_db('compven',$conexion)
---
+++ admin/includes/conexion.php
@@ -0,0 +1,2 @@
+<?php$conexion =mysqli_connect($host_mysql,$user_mysql,$pass_mysql,$db_mysql);
+mysqli_select_db('compven',$conexion)
